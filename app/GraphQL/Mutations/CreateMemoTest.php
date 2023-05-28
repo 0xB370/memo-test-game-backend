e8e5d4614510da981f8fa5b9d063cdc3ed2b9d7a
0xB370 <mansilla_alberto@outlook.com>
2023-05-27 21:16:13 -0300

    CreateMemoTest Adding Images

--- app/GraphQL/Mutations/CreateMemoTest.php
+++ app/GraphQL/Mutations/CreateMemoTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\GraphQL\Mutations;
+use App\Models\Image;
 use App\Models\MemoTest;
 
 final class CreateMemoTest
@@ -15,6 +16,15 @@ final class CreateMemoTest
         $memoTest = new MemoTest();
         $memoTest->name = $args['input']['name']; // Accede directamente a 'name' en el campo 'input'
         $memoTest->save();
+
+        $imageUrls = $args['input']['imageUrls'];
+
+        foreach ($imageUrls as $imageUrl) {
+            $image = new Image();
+            $image->url = $imageUrl;
+            $image->memo_test_id = $memoTest->id; // Asigna el ID del MemoTest creado a la relación
+            $image->save();
+        }
 
         // $memoTest->images = [$args['input']['imageUrls']]; // Accede directamente a 'imageUrls' en el campo 'input'
 
